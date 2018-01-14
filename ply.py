40a0b160e50a458eaef98f31d3b761a4f5c408ce
Archishmaan Peyyety <archishou@gmail.com>
2018-01-14 15:31:04 -0500

    added subcommand init

--- ply.py
+++ ply.py
@@ -1,6 +1,15 @@
 import click
 
 
-@click.command()
-def cli():
-    click.echo("Hello World")
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo('I was invoked without subcommand')
+    else:
+        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
+
+
+@cli.command()
+def init():
+    click.echo('The subcommand')
