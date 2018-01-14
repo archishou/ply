72ce451df90d43df33f75dd95649600675351a9f
Archishmaan Peyyety <archishou@gmail.com>
2018-01-14 16:13:26 -0500

    create dir commands to store all ply commands

--- ply.py
+++ ply.py
@@ -1,15 +1,17 @@
 import click
 
+from commands import initcd
+
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     if ctx.invoked_subcommand is None:
         click.echo('I was invoked without subcommand')
-    else:
-        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
+    if ctx.invoked_subcommand is initcd:
+        click.echo('I am about to invoke init')
 
 
 @cli.command()
 def init():
-    click.echo('The subcommand')
+    initcd.run()
